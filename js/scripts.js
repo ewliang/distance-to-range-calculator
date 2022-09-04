fbc9a1ad958717593d89bbab03a09e2df71c709c
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2022-09-04 09:33:52 -0700

    Cleaned up design.

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ function calculate() {
     const lowerBoundaryNumber = parseFloat(document.getElementById('lowerBoundaryNumberElement').value);
     const upperBoundaryNumber = parseFloat(document.getElementById('upperBoundaryNumberElement').value);
     const currentNumber = parseFloat(document.getElementById('currentNumberElement').value);
-    const distanceFromLowerRangeElement = document.getElementById('distanceFromLowerRangeElement');
-    const distanceFromUpperRangeElement = document.getElementById('distanceFromUpperRangeElement');
+    const distanceFromLowerBoundaryElement = document.getElementById('distanceFromLowerBoundaryElement');
+    const distanceFromUpperBoundaryElement = document.getElementById('distanceFromUpperBoundaryElement');
 
-    let distanceFromLowerRange = currentNumber - lowerBoundaryNumber;
-    let distanceFromUpperRange = upperBoundaryNumber - currentNumber;
-    let percentageDistanceFromLowerRange = ((currentNumber - lowerBoundaryNumber) / (upperBoundaryNumber - lowerBoundaryNumber)) * 100;
-    let percentageDistanceFromUpperRange = ((upperBoundaryNumber - currentNumber) / (upperBoundaryNumber - lowerBoundaryNumber)) * 100;
+    let distanceFromLowerBoundary = currentNumber - lowerBoundaryNumber;
+    let distanceFromUpperBoundary = upperBoundaryNumber - currentNumber;
+    let percentageDistanceFromLowerBoundary = ((currentNumber - lowerBoundaryNumber) / (upperBoundaryNumber - lowerBoundaryNumber)) * 100;
+    let percentageDistanceFromUpperBoundary = ((upperBoundaryNumber - currentNumber) / (upperBoundaryNumber - lowerBoundaryNumber)) * 100;
 
-    distanceFromLowerRangeElement.innerText = distanceFromLowerRange;
-    distanceFromUpperRangeElement.innerText = distanceFromUpperRange;
+    distanceFromLowerBoundaryElement.innerText = distanceFromLowerBoundary;
+    distanceFromUpperBoundaryElement.innerText = distanceFromUpperBoundary;
     rangeDisplayElement.min = lowerBoundaryNumber;
     rangeDisplayElement.max = upperBoundaryNumber;
     rangeDisplayElement.value = currentNumber;
